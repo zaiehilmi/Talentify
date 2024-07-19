8a048dcf43ef0bbac9acd28ec113045e89a576a4
zaiehilmi <zaiehilmi@gmail.com>
2024-07-19 23:11:00 +0800

    build: upgrade project AGP to 8.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
   id("kotlin-parcelize")
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 android {
@@ -49,13 +50,14 @@ android {
 
 dependencies {
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
   implementation("androidx.activity:activity-compose:1.9.0")
   implementation(platform("androidx.compose:compose-bom:2024.06.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
   implementation("dev.olshevski.navigation:reimagined:1.5.0")
 
