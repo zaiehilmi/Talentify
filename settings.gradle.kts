853a7b6d37d1a84ef464bb1a9430510b184877f3
zaiehilmi <zaiehilmi@gmail.com>
2024-07-21 19:36:16 +0800

    style: format kod

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,21 @@
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "Nuna Survei"
 include(":app")
+
+plugins {
+
+}
